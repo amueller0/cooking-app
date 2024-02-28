f9c7608c03de734582c3b02471ada08442d05f3e
Aaron Müller <160637865+amueller0@users.noreply.github.com>
2024-02-28 20:22:17 +0100

    chore: remove hard-coded value

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,7 +6,7 @@ import NavBarItem from "./NavBarItem";
 
 function NavBar() {
     return (
-        <div className="fixed bottom-0 left-0 w-full grid grid-cols-5 bg-black drop-shadow-lg">
+        <div className="fixed bottom-0 left-0 w-full grid grid-flow-col bg-black drop-shadow-lg">
             <NavBarItem path="/" icon={IoMdHome} />
             <NavBarItem path="/search" icon={IoIosSearch} />
             <NavBarItem path="/explore" icon={MdExplore} />
