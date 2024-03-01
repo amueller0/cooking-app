136580fb71acf4d15458f7c22b6cfc2d5a76e918
Aaron Müller <160637865+amueller0@users.noreply.github.com>
2024-03-02 00:26:22 +0100

    style: display line breaks

--- src/app/recipe/[recipeId]/page.tsx
+++ src/app/recipe/[recipeId]/page.tsx
@@ -33,11 +33,21 @@ export default async function RecipePage({ params }: RecipePageProps) {
 
             <div className="mt-8 p-2 rounded-md bg-white/10">
                 <h2 className="text-xl font-semibold">Ingredients</h2>
-                <div className="mt-2">{recipe.ingredients}</div>
+                <div
+                    className="mt-2"
+                    dangerouslySetInnerHTML={{
+                        __html: recipe.ingredients.replace(/\n/g, "<br />")
+                    }}
+                />
             </div>
             <div className="mt-8 p-2 rounded-md bg-white/10">
                 <h2 className="text-xl font-semibold">Instructions</h2>
-                <div className="mt-2">{recipe.instructions}</div>
+                <div
+                    className="mt-2"
+                    dangerouslySetInnerHTML={{
+                        __html: recipe.instructions.replace(/\n/g, "<br />")
+                    }}
+                />
             </div>
 
             <div className="h-[1px] my-12 bg-white/20" />
